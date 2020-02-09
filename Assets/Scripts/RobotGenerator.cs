badab385b80e02186b29b83e923f9440add04356
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-09 11:29:21 +0900

    ロボの配置
    配列を利用し、各ポジションにロボを配置

---
+++ Assets/Scripts/RobotGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    List<RobotGroup> groups = new List<RobotGroup>();
+    void Start()
+    {
+        for (int cnt = 0; cnt<2; cnt++)  //2つのグループ作成
+        {
+            groups.Add(new RobotGroup(prefab, 0.2f + 0.4f * cnt, 0.3f));
+        }
+
+        for(int cnt=0;cnt<2;cnt++)
+        {
+            groups.Add(new RobotGroup(prefab,0.2f + 0.4f * cnt, 0.3f));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RobotGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// : MonoBehaviour start,Updateメソッドを使わない場合MonoBehabiourクラスを継承する必要がないので削除して良い
+public class RobotGroup{
+
+    List<RobotController> robots = new List<RobotController>();
+
+    public RobotGroup(GameObject prefab, float px, float py)  //インスタンス生成された時に呼ばれるメソッド、コンストラクター、クラス名と同じメソッドにより作成出来る
+    {
+        //計９回のループ
+        for(int x = 0; x<3; x++)
+        {
+            for(int y = 0; y<3; y++)
+            {
+                Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(px + 0.1f * x, py + 0.13f * y, 0));  //ビューポートからワールド座標への変換
+                p.z = 0;
+                GameObject obj = Object.Instantiate(prefab, p, Quaternion.identity);
+                robots.Add(obj.GetComponent<RobotController>());  //生成したロボットの保存
+            }
+        }
+    }
+}
+
+
+
+
+
